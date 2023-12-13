092858a6711e8c1312acb66eec6927b0b27856e7
KTB2110 <70537643+KTB2110@users.noreply.github.com>
2023-12-12 20:21:09 -0500

    Update gpt4_integration.py
    Changed gpt4 to gpt4 turbo vison preview

--- gpt4_integration.py
+++ gpt4_integration.py
@@ -3,26 +3,28 @@ import json
 import os
 from PySide2 import QtWidgets
 
-def ask_for_api_key():
-    api_key, ok = QtWidgets.QInputDialog.getText(None, "API Key", "Enter your OpenAI API key:")
-    if ok and api_key:
-        with open(os.path.join(os.path.expanduser("~"), "api_key.txt"), "w") as api_key_file:
-            api_key_file.write(api_key)
-        return api_key
-    else:
-        raise ValueError("API key not provided. Please enter a valid OpenAI API key.")
+# def ask_for_api_key():
+#     api_key, ok = QtWidgets.QInputDialog.getText(None, "API Key", "Enter your OpenAI API key:")
+#     if ok and api_key:
+#         with open(os.path.join(os.path.expanduser("~"), "api_key.txt"), "w") as api_key_file:
+#             api_key_file.write(api_key)
+#         return api_key
+#     else:
+#         raise ValueError("API key not provided. Please enter a valid OpenAI API key.")
 
-# Read API key from file or ask the user for it
-api_key_path = os.path.join(os.path.expanduser("~"), "api_key.txt")
-if os.path.exists(api_key_path):
-    with open(api_key_path, "r") as api_key_file:
-        API_KEY = api_key_file.read().strip()
-else:
-    API_KEY = ask_for_api_key()
+# # Read API key from file or ask the user for it
+# api_key_path = os.path.join(os.path.expanduser("~"), "api_key.txt")
+# if os.path.exists(api_key_path):
+#     with open(api_key_path, "r") as api_key_file:
+#         API_KEY = api_key_file.read().strip()
+# else:
+#     API_KEY = ask_for_api_key()
+
+API_KEY = "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"
 
 API_ENDPOINT = "https://api.openai.com/v1/chat/completions"
 
-def generate_chat_completion(messages, model="gpt-4", temperature=0.3, max_tokens=None):
+def generate_chat_completion(messages, model="gpt-4-vision-preview", temperature=0.3, max_tokens=None):
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {API_KEY}",
